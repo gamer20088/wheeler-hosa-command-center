ce99fd970a9ff6cc98d5e4d003aa2e664a203eb2
gamer20088 <46035878+gamer20088@users.noreply.github.com>
2026-05-22 23:08:20 -0400

    Wrap app with navigation simplifier

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import StudentPortal from './StudentPortal.jsx'
 import { LanguageShell } from './LanguageShell.jsx'
+import { NavigationSimplifier } from './NavigationSimplifier.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageShell>
-      <StudentPortal />
+      <NavigationSimplifier>
+        <StudentPortal />
+      </NavigationSimplifier>
     </LanguageShell>
   </StrictMode>,
 )
